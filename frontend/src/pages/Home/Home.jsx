cab49a266a9a07eccd2bdea32552b2c376303827
Pakhi-Rajpoot <pakhirajpootmpi@gmail.com>
2025-11-12 22:42:09 +0530

    header

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import './Home.css';
+import Header from '../../components/Header/Header';
 const Home = () => {
   return (
     <div>
+      <Header/>
 
     </div>
   )
